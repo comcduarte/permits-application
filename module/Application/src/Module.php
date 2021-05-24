be8cc3cfd085feb4cd145740917966edde195d6d
DuarteC <DuarteC@IT-WKS017.MidNet.CityOfMiddletown.com>
2021-05-24 11:44:57 -0400

    Initial Commit

---
+++ module/Application/src/Module.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @see       https://github.com/laminas/laminas-mvc-skeleton for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-mvc-skeleton/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-mvc-skeleton/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Application;
+
+class Module
+{
+    const TITLE = "Permits";
+    const VERSION = "v0.0.0";
+    
+    public function getConfig() : array
+    {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+}
